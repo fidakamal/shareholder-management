5c73a5d0b775faf070f127dd20baace2b60dccfa
Fida <n.fida6941@gmail.com>
2026-08-27 22:02:36 +0600

    Added shareholderDTO.

--- src/main/java/com/example/shareholder_management/shareholder/ShareholderController.java
+++ src/main/java/com/example/shareholder_management/shareholder/ShareholderController.java
@@ -1,13 +1,30 @@
 package com.example.shareholder_management.shareholder;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ShareholderController {
 
     @GetMapping("/shareholders/new")
-    public String showShareholderForm() {
+    public String showShareholderForm(Model model) {
+
+        model.addAttribute("shareholder", new ShareholderDTO());
+
+        return "shareholder/form";
+    }
+
+    @PostMapping("/shareholders")
+    public String submitShareholder(ShareholderDTO shareholder) {
+
+        System.out.println("Folio/BO: " + shareholder.getFolioBo());
+        System.out.println("Name: " + shareholder.getShareholderName());
+        System.out.println("Phone: " + shareholder.getPhone());
+        System.out.println("Email: " + shareholder.getEmail());
+
         return "shareholder/form";
     }
 }
---
+++ src/main/java/com/example/shareholder_management/shareholder/ShareholderDTO.java
@@ -0,0 +1,213 @@
+package com.example.shareholder_management.shareholder;
+
+
+
+public class ShareholderDTO {
+
+    private String folioBo;
+    private Integer customerType;
+    private String shareholderName;
+    private String representative;
+
+    private Integer citizenType;
+    private Integer isEmployee;
+    private String residenceType;
+
+    private String nid;
+    private String phone;
+    private String tin;
+    private String email;
+
+    private String registrationDate;
+    private Integer icbCode;
+
+    private String add1;
+    private String add2;
+    private String add3;
+    private String add4;
+    private String countryName;
+
+    private String bankName;
+    private String branchName;
+    private String accountNumber;
+    private String routingNumber;
+
+
+    // Getters and Setters
+
+    public String getFolioBo() {
+        return folioBo;
+    }
+
+    public void setFolioBo(String folioBo) {
+        this.folioBo = folioBo;
+    }
+
+    public Integer getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(Integer customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getShareholderName() {
+        return shareholderName;
+    }
+
+    public void setShareholderName(String shareholderName) {
+        this.shareholderName = shareholderName;
+    }
+
+    public String getRepresentative() {
+        return representative;
+    }
+
+    public void setRepresentative(String representative) {
+        this.representative = representative;
+    }
+
+    public Integer getCitizenType() {
+        return citizenType;
+    }
+
+    public void setCitizenType(Integer citizenType) {
+        this.citizenType = citizenType;
+    }
+
+    public Integer getIsEmployee() {
+        return isEmployee;
+    }
+
+    public void setIsEmployee(Integer isEmployee) {
+        this.isEmployee = isEmployee;
+    }
+
+    public String getResidenceType() {
+        return residenceType;
+    }
+
+    public void setResidenceType(String residenceType) {
+        this.residenceType = residenceType;
+    }
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTin() {
+        return tin;
+    }
+
+    public void setTin(String tin) {
+        this.tin = tin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Integer getIcbCode() {
+        return icbCode;
+    }
+
+    public void setIcbCode(Integer icbCode) {
+        this.icbCode = icbCode;
+    }
+
+    public String getAdd1() {
+        return add1;
+    }
+
+    public void setAdd1(String add1) {
+        this.add1 = add1;
+    }
+
+    public String getAdd2() {
+        return add2;
+    }
+
+    public void setAdd2(String add2) {
+        this.add2 = add2;
+    }
+
+    public String getAdd3() {
+        return add3;
+    }
+
+    public void setAdd3(String add3) {
+        this.add3 = add3;
+    }
+
+    public String getAdd4() {
+        return add4;
+    }
+
+    public void setAdd4(String add4) {
+        this.add4 = add4;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+}
