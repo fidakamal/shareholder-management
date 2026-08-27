47975b2e85bc59f6053b191f34ccd22bdbe6085a
Fida <n.fida6941@gmail.com>
2026-08-27 19:28:21 +0600

    Initial commit

---
+++ src/test/java/com/example/shareholder_management/ShareholderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareholder_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareholderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
