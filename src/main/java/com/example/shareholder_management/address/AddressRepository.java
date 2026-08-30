d8d854456cf70b7ff34f01ffdec03b373de3bf6e
Fida <n.fida6941@gmail.com>
2026-08-30 21:22:59 +0600

    Added shareholder and address repository.

---
+++ src/main/java/com/example/shareholder_management/address/AddressRepository.java
@@ -0,0 +1,6 @@
+package com.example.shareholder_management.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, String> {
+}
---
+++ src/main/java/com/example/shareholder_management/shareholder/ShareholderRepository.java
@@ -0,0 +1,7 @@
+
+package com.example.shareholder_management.shareholder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShareholderRepository extends JpaRepository<Shareholder, String> {
+}
